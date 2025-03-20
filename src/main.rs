03a7c3827b8e92a5df389cbe40384ef5719365e0
Long Le <tlenguye@emich.edu>
2025-03-20 10:17:17 -0400

    actix template

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,19 @@ let movieSearchBox = document.getElementById("movie-search-box");
 let searchList = document.getElementById("search-list");
 let resultGrid = document.getElementById("result-grid");
 
-async function 
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
