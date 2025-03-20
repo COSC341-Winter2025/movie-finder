a3802608f2c45c8e93be7c18c9c75edec29e838e
Long Le <tlenguye@emich.edu>
2025-03-20 15:40:25 -0400

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,13 @@ struct MovieApiResponse{
 async fn search_movies(query: web::Path<String>) -> impl Responder {
     let api_key = env::var("MOVIE_API_KEY").expect("API key not found in .env");
     let url = format!("http://www.omdbapi.com/?apikey={}&s={}", api_key, query);
+
+    match reqwest::get(&url).await {
+        Ok(response) => match response.json::<MovieApiResponse>().await {
+            Ok(data) => HttpResponse::Ok().json(data.Search),
+            Err(_) => HttpResponse::InternalServerError().body("Error parsing movie data"),
+        },
+        Err(_) => HttpResponse::InternalServerError().body("Error fetching movie data"),
+        }
 }
 
