5068d82a1e9a34ec813e24a430dfc804a55eb523
Long Le <tlenguye@emich.edu>
2025-03-20 16:41:04 -0400

    function loadMovies

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const searchBox = document.getElementById("movie-search-box");
+  const movieSearchBox = document.getElementById("movie-search-box");
+  const searchList = document.getElementById("search-list");
   const resultGrid = document.getElementById("result-grid");
+
+  // load movies from API
+  async function loadMovies(searchTerm) {
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=bc8d5ea4`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if (data.Response === "True") {
+      displayMovies(data.Search);
+    }
+  }
 
   // Event listener for the search box
   searchBox.addEventListener("keyup", async () => {
