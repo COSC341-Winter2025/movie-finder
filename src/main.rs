de6681985153234ff2e666154785a4c695d678de
Long Le <tlenguye@emich.edu>
2025-03-20 15:33:18 -0400

    declare struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-fn main() {
-    
-}
-let movieSearchBox = document.getElementById("movie-search-box");
-let searchList = document.getElementById("search-list");
-let resultGrid = document.getElementById("result-grid");
-
-use actix_web::{get, web, App, HttpServer, Responder};
-
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
-
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(greet)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[derive(Serialize, Deserialize)]
+struct Movie {
+    title: String,
+    year: String,
+    imdb_id: String,
+    poster: String,
 }
