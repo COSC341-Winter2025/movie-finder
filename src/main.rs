7b89fa431068562eb573a5cc8ca64939b060aa71
Long Le <tlenguye@emich.edu>
2025-03-21 09:19:43 -0400

    api works

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,50 @@ use serde::{Deserialize, Serialize};
 use std::env;
 use reqwest;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Movie {
+    #[serde(rename = "Title")]
     title: String,
+
+    #[serde(rename = "Year")]
     year: String,
+
+    #[serde(rename = "imdbID")]
     imdb_id: String,
+
+    #[serde(rename = "Poster")]
     poster: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct MovieApiResponse{
-    Search: Vec<Movie>,
+    #[serde(rename = "Search")]
+    search: Vec<Movie>,
     
 }
 
 async fn search_movies(query: web::Path<String>) -> impl Responder {
     let api_key = env::var("MOVIE_API_KEY").expect("API key not found in .env");
-    let url = format!("http://www.omdbapi.com/?s={}&apikey={}", query,api_key);
+    let url = format!("http://www.omdbapi.com/?s={}&apikey={}", query, api_key);
+
+    println!("Fetching movies from: {}", url); // Log API call
 
     match reqwest::get(&url).await {
         Ok(response) => match response.json::<MovieApiResponse>().await {
-            Ok(data) => HttpResponse::Ok().json(data.Search),
-            Err(_) => HttpResponse::InternalServerError().body("Error parsing movie data"),
+            Ok(data) => {
+                println!("Received data: {:?}", data.search);
+                HttpResponse::Ok().json(data.search)
+            }
+
+            Err(_) =>{
+                println!("Error parsing movie data");
+                HttpResponse::InternalServerError().body("Error parsing movie data")
+            } 
         },
-        Err(_) => HttpResponse::InternalServerError().body("Error fetching movie data"),
+        Err(_) =>{
+            println!("Error fetching movie data");
+            HttpResponse::InternalServerError().body("Error fetching movie data")
+        } 
         }
 }
 
